8a6b9bb640fb7fa39fda3d0f00f289cfb167967a
Victor Evdokimov <ext_rem@list.ru>
2021-05-05 00:13:40 +0300

    initial calc

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,42 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div class="display">
+      <input v-model.number="operand1" placeholder="введите число"/>
+      <input v-model.number="operand2" placeholder="введите число"/>
+      = {{ result }}
+    </div>
+    <div class="keyboard">
+      <button @click="res($event)">+</button>
+      <button @click="res($event)">-</button>
+      <button @click="res($event)">*</button>
+      <button @click="res($event)">/</button>
+      <button @click="res($event)">**</button>
+      <button @click="res($event)">деление без остатка</button>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  export default {
+    name: 'calculator',
+    data: () => ({
+        operand1: '',
+        operand2: '', 
+        result: 0
+    }),
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+    methods: {
+      res(event) {
+        if (event.target.innerText == "деление без остатка") {
+          return this.result = Math.floor(this.operand1 / this.operand2);
+        } else
+        return this.result = eval(`${this.operand1 + event.target.innerText + this.operand2}`);
+      },
+    }
   }
-}
 </script>
 
-<style lang="scss">
+// <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
