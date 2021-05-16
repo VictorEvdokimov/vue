7db869882ed22147832d53664d835455aa20fe0c
Victor Evdokimov <ext_rem@list.ru>
2021-05-16 18:40:39 +0300

    lesson2

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,26 @@
 <template>
   <div>
     <div class="display">
-      <input v-model.number="operand1" placeholder="введите число"/>
-      <input v-model.number="operand2" placeholder="введите число"/>
+      <input v-model="operand1" placeholder="введите число" ref="op1"/>
+      <input v-model="operand2" placeholder="введите число" ref="op2"/>
       = {{ result }}
     </div>
     <div class="keyboard">
-      <button @click="res($event)" :disabled="disabledAllButton">+</button>
-      <button @click="res($event)" :disabled="disabledAllButton">-</button>
-      <button @click="res($event)" :disabled="disabledAllButton">*</button>
-      <button @click="res($event)" :disabled="disabledAllButton">/</button>
-      <button @click="res($event)" :disabled="disabledAllButton">**</button>
-      <button @click="res($event)" :disabled="disabledAllButton">деление без остатка</button>
+      <button v-for='(operand, index) in operands' :key="index" @click="res($event)" :disabled="disabledAllButton">{{ operand }}</button>
+    </div>
+    <div>
+      <input type="checkbox" id="checkbox" v-model="checked" @click="changeChecked">
+      <label for="checkbox">Отобразить экранную клавиатуру</label>
+    </div>
+    <div v-show="checked">
+      <button v-for="(simbol, index) in keyboard" :key="index" @click="inputNumber(simbol)">{{ simbol }}</button>
+      <button @click="backspace">&lt;--</button>
+    </div>
+    <div v-show="checked">
+      <input name="input" type="radio" id="one" value="Один" v-model="radio" @change="enableRadio()">
+      <label for="one">Число 1</label>
+      <input name="input" type="radio" id="two" value="Два" v-model="radio" @change="enableRadio()">
+      <label for="two">Число 2</label>
     </div>
   </div>
 </template>
@@ -20,9 +29,13 @@
   export default {
     name: 'calculator',
     data: () => ({
-        operand1: '',
-        operand2: '', 
-        result: 0
+        operand1: "",
+        operand2: "", 
+        result: 0,
+        operands: ['+', '-', '*', '/', '**', 'деление без остатка'],
+        keyboard: [...Array(10).keys()],
+        checked: false,
+        radio: "Один"
     }),
 
     methods: {
@@ -32,18 +45,50 @@
         } else
         return this.result = eval(`${this.operand1 + event.target.innerText + this.operand2}`);
       },
+
+      backspace() {
+        if (this.radio === 'Один') {
+          this.operand1 = this.operand1.substring(0, this.operand1.length-1)
+        } else {
+          this.operand2 = this.operand2.substring(0, this.operand2.length-1)
+        }
+      },
+       
+      changeChecked() {
+        this.checked=!this.checked;
+      },
+
+      enableRadio() {
+        if(this.radio === 'Один') {
+          this.$refs.op1.focus();
+        } else {
+          this.$refs.op2.focus();
+        }
+      },
+      inputNumber(simbol) {
+       
+        if(this.radio === 'Один') {
+          this.operand1 = this.operand1 + simbol;
+         
+        
+        } else {
+          this.operand2 = this.operand2 + simbol;
+          console.log(typeof this.operand2)
+        
+        }
+      }
     },
 
     computed: {
       disabledAllButton: function () {
-        if ((this.operand1.length != 0 && this.operand2.length != 0) && 
-        ((typeof this.operand1 == "number") && (typeof this.operand2 == "number"))) {
+        if (this.operand1.length != 0 && this.operand2.length != 0) {
           return false;
         }
         return true;
       }
     }
   }
+  
 </script>
 
 // <style lang="scss">
